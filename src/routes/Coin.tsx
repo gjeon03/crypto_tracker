1b86d88db270e1cd8dbec45ff134aa5e05964a44
gjeon <gjeon0321@gmail.com>
2021-12-12 02:58:28 +0900

    styles setup

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,5 +1,14 @@
 import {useParams} from "react-router";
 
+// interface RouteParams {
+// 	coinId: string;
+// }
+// function Coin() {
+// 	const { coinId } = useParams<keyof RouteParams>() as RouteParams;
+// 	return <h1>Coin: {coinId}</h1>;
+// }
+// 어떤 params가 들어오던 다 string이기에 지정을 안해줘도 에러가 안뜨나?
+
 function Coin() {
 	const {coinId} = useParams();
 	return <h1>Coin: {coinId}</h1>
